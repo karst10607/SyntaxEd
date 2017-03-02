bb1522c33bd8c91e7a155c1ebf6e02af5b845693
Howl <karst10607@gmail.com>
2017-03-02 14:54:07 +0800

    modified, now the phone can vibrate
    cordova js that vibrates

---
+++ Phone4/js/vibration.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+    $(document).on("click", "#vibrate", function() {
+        var time = parseInt($("#time").val());
+        navigator.vibrate(time);
+        //Max time for Windows and Blackberry: 5000ms, exceeding value will be truncated.
+
+        alert("test:"+time);
+        $("#cancelVibration").button("enable");
+        setTimeout(function(){
+            $("#cancelVibration").button("disable");
+        }, time);
+    });
+
+    $(document).on("click", "#cancelVibration", function() {
+        navigator.vibrate(0);
+        //Or navigator.vibrate([])
+        //Or navigator.vibrate([0])
+
+        $("#cancelVibration").button("disable");
+    });
+
+}, false);
